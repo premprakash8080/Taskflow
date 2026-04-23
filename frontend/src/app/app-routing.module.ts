02d37b4f957d3abb80737808869e61c4e10fbd8e
premprakash yadav <84430639+premprakash8080@users.noreply.github.com>
2026-04-24 00:36:57 +0530

    Inintial Commit

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomLayoutComponent } from './custom-layout/custom-layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CustomLayoutComponent,
+    children: []
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    // preloadingStrategy: PreloadAllModules,
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled'
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
