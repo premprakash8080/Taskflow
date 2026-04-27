f01765437985ab3e5ae52dc9b1fada93d4c83f81
premprakash yadav <84430639+premprakash8080@users.noreply.github.com>
2026-04-27 23:58:50 +0530

    Fixed the registration token issue

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -1,58 +1,48 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { User } = require('../models/index');
-
 //Register 
-
 const register = async (req, res) => {
     try {
         const { name, email, password } = req.body;
-
-
         if (!name || !email || !password) {
             return res.status(400).json({ message: 'Name, email and password are required.' });
         }
-
-
         const existingUser = await User.findOne({ where: { email } });
         if (existingUser) {
             return res.status(409).json({ message: 'Email already registered.' });
         }
-
-
         const password_hash = await bcrypt.hash(password, 10);
-
-
         const user = await User.create({ name, email, password_hash });
-
-
+        const token = jwt.sign(
+            {
+                id: user.id, email: user.email
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.JWT_EXPIRES_IN }
+        );
         return res.status(201).json({
             message: 'User registered successfully.',
+            token,
             user: {
                 id: user.id,
                 name: user.name,
                 email: user.email,
             },
         });
-
     } catch (error) {
         return res.status(500).json({ message: 'Server error', error: error.message });
     }
 };
-
 // Login
-
-
 const login = async (req, res) => {
     console.log('Login req.body:', req.body); // ⬅️ Add karo
     try {
         const { email, password } = req.body;
-
         if (!email || !password) {
             console.log('Validation failed');
             return res.status(400).json({ message: 'Email and Password are Required' });
         }
-
         const user = await User.findOne({
             where: { email }
         });
@@ -61,13 +51,11 @@ const login = async (req, res) => {
             console.log('User not found');
             return res.status(400).json({ message: 'Invalid email or password' });
         }
-
         const isMatch = await bcrypt.compare(password, user.password_hash);
         console.log('Password match:', isMatch);
         if (!isMatch) {
             return res.status(401).json({ message: ' Invalid email or password' })
         };
-
         const token = jwt.sign(
             {
                 id: user.id, email: user.email
@@ -75,8 +63,6 @@ const login = async (req, res) => {
             process.env.JWT_SECRET,
             { expiresIn: process.env.JWT_EXPIRES_IN }
         );
-
-
         return res.status(200).json({
             message: 'Login successful.',
             token,
@@ -88,12 +74,9 @@ const login = async (req, res) => {
         });
     } catch (error) {
         return res.status(500).json({ message: 'Server error.', error: error.message });
-
     }
 }
-
 // Logout
-
 const logout = async (req, res) => {
     try {
         return res.status(200).json({ message: 'Logged out successfully.' });
@@ -101,11 +84,7 @@ const logout = async (req, res) => {
         return res.status(500).json({ message: 'Server error.', error: error.message });
     }
 };
-
-
 // Get Current User
-
-
 const getCurrentUser = async (req, res) => {
     try {
         return res.status(200).json({
@@ -121,5 +100,4 @@ const getCurrentUser = async (req, res) => {
         return res.status(500).json({ message: 'Server error.', error: error.message });
     }
 };
-
 module.exports = { register, login, logout, getCurrentUser };
